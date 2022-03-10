22086d67ec693311f225ecb1583b53f42e88292c
victorsalomao <victor.salomaor@gmail.com>
2022-03-10 11:47:46 -0300

    primeiro commit do jamil

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+
+const variavel_Dolar = document.querySelector();
+const variavel_Real = document.querySelector();
